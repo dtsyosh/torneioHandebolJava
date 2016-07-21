470b82ddfdc2c5269f29f01a994eec0088eb2588
yosh <yosh@yosh-Inspiron-5458>
2016-07-21 12:06:56 -0400

    Adicionado atributos e métodos Getters and Setters.

--- Torneio Handebol/src/modelo/Time.java
+++ Torneio Handebol/src/modelo/Time.java
@@ -11,4 +11,73 @@ package modelo;
  */
 public class Time {
     
+    private String nome;
+    private int numeroVitorias;
+    private int numeroDerrotas;
+    private int numeroEmpates;
+    private int numeroGolsMarcados;
+    private int numeroGolsSofridos;
+    private int pontos;
+
+    public Time(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumeroVitorias() {
+        return numeroVitorias;
+    }
+
+    public void setNumeroVitorias(int numeroVitorias) {
+        this.numeroVitorias = numeroVitorias;
+    }
+
+    public int getNumeroDerrotas() {
+        return numeroDerrotas;
+    }
+
+    public void setNumeroDerrotas(int numeroDerrotas) {
+        this.numeroDerrotas = numeroDerrotas;
+    }
+
+    public int getNumeroEmpates() {
+        return numeroEmpates;
+    }
+
+    public void setNumeroEmpates(int numeroEmpates) {
+        this.numeroEmpates = numeroEmpates;
+    }
+
+    public int getNumeroGolsMarcados() {
+        return numeroGolsMarcados;
+    }
+
+    public void setNumeroGolsMarcados(int numeroGolsMarcados) {
+        this.numeroGolsMarcados = numeroGolsMarcados;
+    }
+
+    public int getNumeroGolsSofridos() {
+        return numeroGolsSofridos;
+    }
+
+    public void setNumeroGolsSofridos(int numeroGolsSofridos) {
+        this.numeroGolsSofridos = numeroGolsSofridos;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+    
+    
 }
