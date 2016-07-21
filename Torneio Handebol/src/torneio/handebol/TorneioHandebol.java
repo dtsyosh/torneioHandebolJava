0d5d8fb7b4843eeb4ac7db742841d9ec8ae3ef45
yosh <yosh@yosh-Inspiron-5458>
2016-07-21 12:35:38 -0400

    Implementado a leitura dos 12 times assim como a instanciação dos respectivos objetos.

--- Torneio Handebol/src/torneio/handebol/TorneioHandebol.java
+++ Torneio Handebol/src/torneio/handebol/TorneioHandebol.java
@@ -5,6 +5,11 @@
  */
 package torneio.handebol;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import modelo.Time;
+
 /**
  *
  * @author yosh
@@ -16,6 +21,17 @@ public class TorneioHandebol {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        List<Time> listaTimes = new ArrayList();
+        Scanner ler = new Scanner(System.in);
+        
+        
+        System.out.println("Digite o nome dos 12 times que disputarão o torneio.");
+        for(int i = 1; i <= 12; i++){
+            System.out.println("Time " + i + ": ");
+            listaTimes.add(new Time(ler.nextLine()));
+        }
     }
-    
+    private void realizarJogos(List<Time> listaTimes){
+        
+    }
 }
