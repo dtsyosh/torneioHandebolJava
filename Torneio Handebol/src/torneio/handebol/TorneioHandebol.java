5a252ba300b97108d8ba3e42f9cce8d0bccecbad
yosh <yosh@yosh-Inspiron-5458>
2016-07-21 14:15:47 -0400

    Corrigindo formatação dos prints

--- Torneio Handebol/src/torneio/handebol/TorneioHandebol.java
+++ Torneio Handebol/src/torneio/handebol/TorneioHandebol.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package torneio.handebol;
 
 import java.util.ArrayList;
@@ -11,17 +6,9 @@ import java.util.List;
 import java.util.Scanner;
 import modelo.Time;
 
-/**
- *
- * @author yosh
- */
 public class TorneioHandebol {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         List<Time> listaTimes = new ArrayList();
         Scanner ler = new Scanner(System.in);
 
@@ -34,36 +21,36 @@ public class TorneioHandebol {
         realizarJogos(listaTimes);  //Realizando os jogos entre os times
         Collections.sort(listaTimes); //Ordenando a lista de times (tabela)
 
-        System.out.println("Qual relatorio deseja exibir? ->[1~3]<- pressione 0 para sair");
+        System.out.print("Qual relatorio deseja exibir? ->[1~3]<- pressione 0 para sair: ");
         int opcao = ler.nextInt();
         Time t;
         while (opcao != 0) {
             switch (opcao) {
                 case 1:
-                    System.out.println("   Times              V    E    D   GM   GS  Pontos");
+                    System.out.println("Times                   V    E    D   GM   GS  Pontos");
                     for (int i = 0; i < 3; i++) {
                         t = listaTimes.get(i);
-                        System.out.printf("%d° %-10s %9d %4d %4d %4d %4d %5d\n", i + 1, t.getNome(), t.getVitorias(), t.getEmpates(), t.getDerrotas(),
+                        System.out.printf("%-15s %9d %4d %4d %4d %4d %5d\n",t.getNome(), t.getVitorias(), t.getEmpates(), t.getDerrotas(),
                                 t.getGolsMarcados(), t.getGolsSofridos(), t.getPontos());
                     }
                     break;
                 case 2:
-                    System.out.println("    Times              V    E    D   GM   GS  Pontos");
+                    System.out.println("Times                   V    E    D   GM   GS  Pontos");
                     for (int i = listaTimes.size() - 3; i < listaTimes.size(); i++) {
                         t = listaTimes.get(i);
-                        System.out.printf("%d° %-10s %9d %4d %4d %4d %4d %5d\n", i + 1, t.getNome(), t.getVitorias(), t.getEmpates(), t.getDerrotas(),
+                        System.out.printf("%-15s %9d %4d %4d %4d %4d %5d\n", t.getNome(), t.getVitorias(), t.getEmpates(), t.getDerrotas(),
                                 t.getGolsMarcados(), t.getGolsSofridos(), t.getPontos());
                     }
                     break;
                 case 3:
-                    System.out.println("Times              V    E    D   GM   GS  Pontos");
+                    System.out.println("Times                   V    E    D   GM   GS  Pontos");
                     for (int i = 0; i < listaTimes.size(); i++) {
                         t = listaTimes.get(i);
-                        System.out.printf("%-10s %9d %4d %4d %4d %4d %5d\n", t.getNome(), t.getVitorias(), t.getEmpates(), t.getDerrotas(),
+                        System.out.printf("%-15s %9d %4d %4d %4d %4d %5d\n", t.getNome(), t.getVitorias(), t.getEmpates(), t.getDerrotas(),
                                 t.getGolsMarcados(), t.getGolsSofridos(), t.getPontos());
                     }
             }
-            System.out.println("Qual relatorio deseja exibir? ->[1~3]<- pressione 0 para sair");
+            System.out.print("Qual relatorio deseja exibir? ->[1~3]<- pressione 0 para sair: ");
             opcao = ler.nextInt();
 
         }
